bcb5864d2f28d6dcb4da5c03265ba19573cba81b
João Vertelo <joaovertelo@gmail.com>
2020-06-28 20:34:15 -0300

    adicioando chamada do backend para listar projetos

--- conceitos/frontend/src/App.js
+++ conceitos/frontend/src/App.js
@@ -1,25 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import api from './services/Api'
 
 import './App.css'
-import backgroundImage from './assets/background.jpeg'
-
 import Header from './components/Header'
 
 
 const App = () =>  {
 
-    const [texto, setTexto] = useState('meu texto');
+    const [projects, setProjects] = useState([]);
+    
 
+    useEffect(() => {
+        api.get('projects').then(res => {
+            setProjects(res.data)
+        })
+    }, [])
 
-    const handleAdd = () => {
-        setTexto(texto + "novoTexto");
+    async function handleAdd(){
+        const response = await api.post('projects', {title: "oii", owner: "Jao"});
+        console.log(response.data)
+        setProjects([...projects, response.data]);
+        console.log(projects)
     }
 
     return <>
         <Header title="texto" > 
         
-        <div> {texto}</div>
-        <img width={500} height={500} src={backgroundImage} />
+        <ul>
+            {projects && projects.map(project => (
+                <li key={project.id}>
+                    {project.title} - {project.owner}
+            </li>))}
+        </ul>
         <button type="button" onClick={handleAdd} > Add </button> 
         </Header>
     </>
