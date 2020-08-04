54c2cc486cf185a18b37c83b23022f7f7834b0db
João Vertelo <joaovertelo@gmail.com>
2020-08-03 21:40:59 -0300

    criando tela de login app

--- conceitos/appgobarber/src/pages/SingIn/index.tsx
+++ conceitos/appgobarber/src/pages/SingIn/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Image } from 'react-native'
 
-import { Container } from './styles'
+import logoImg from '../../assets/logo.png'
+
+import { Container, Title } from './styles'
+
+import Input from '../../components/input'
+import Button from '../../components/button'
 
 const SignIn: React.FC = () => {
     return (
-        <Container/>
+        <Container>
+            <Image source={logoImg} />
+
+            <Title> Faça seu logon</Title>
+
+            <Input name="email" icon="mail" placeholder="E-Mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button onPress={() => { }}>Entrar</Button>
+        </Container>
     )
 }
 
--- conceitos/appgobarber/src/pages/SingIn/styles.ts
+++ conceitos/appgobarber/src/pages/SingIn/styles.ts
@@ -1,8 +1,17 @@
 import styled from 'styled-components/native'
 
 const Container = styled.View`
-
-    
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    padding: 0 30px;
 `;
 
-export { Container };
+const Title = styled.Text`
+    font-size: 24px;
+    color: #f4ede8;
+    font-family: 'RobotoSlab-Medium';
+    margin: 64px 0 24px;
+`;
+
+export { Container, Title };
